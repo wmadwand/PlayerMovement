1b845bfc085df349fdba2123acdddec8f8e20f5c
wmadwand <wmadwand@gmail.com>
2022-11-13 16:59:41 +0500

    Some tweaks

--- Assets/BasciInput.cs
+++ Assets/BasciInput.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class BasciInput : MonoBehaviour
 {
-    public float moveSpeed = 5;
-    public float rotSpeed = 5;
+    [SerializeField] private float walkSpeed = 5;
+    [SerializeField] private float sprintSpeed = 5;
+    [SerializeField] private float rotSpeed = 5;
 
-    private Animator animator;
-    private Rigidbody rigidbody;
+    [SerializeField] private string _animatorIsGrounded;
+    [SerializeField] private string _animatorIsRolling;
+    [SerializeField] private string _animatorSpeed;
+    [SerializeField] private string _animatorWalk;
 
-    Vector3 direction;
+    [Header("Keybinds")]
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode rollKey = KeyCode.LeftAlt;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask WhatIsGround;
+    public bool grounded;
+
+    public MovementState state;
+
+    private float _moveSpeed;
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+
+    private Vector3 _direction;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Start()
     {
-        rigidbody.freezeRotation = true;
+        _rigidbody.freezeRotation = true;
     }
 
     private void Update()
@@ -28,17 +47,52 @@ public class BasciInput : MonoBehaviour
         var hor = Input.GetAxisRaw("Horizontal");
         var ver = Input.GetAxisRaw("Vertical");
 
-        direction = new Vector3(-ver, 0, hor).normalized;
+        _direction = new Vector3(-ver, 0, hor).normalized;
+
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, WhatIsGround);
+
+        if (grounded)
+        {
+            _animator.SetTrigger(_animatorIsGrounded);
+        }
+
+        StateHandler();
+    }
+
+    private void StateHandler()
+    {
+        //Mode - Sprinting
+        if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            _moveSpeed = sprintSpeed;
+        }
+
+        //Mode - walking
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            _moveSpeed = walkSpeed;
+
+            _animator.SetTrigger(_animatorIsGrounded);
+        }
     }
 
     private void FixedUpdate()
     {
-        rigidbody.velocity = direction * moveSpeed * Time.deltaTime;
+        _rigidbody.velocity = _direction * _moveSpeed;
 
-        if (direction == Vector3.zero) { return; }
+        if (_direction == Vector3.zero) { return; }
 
-        var targetRot = Quaternion.LookRotation(direction, Vector3.up);
-        var resultRot = Quaternion.Slerp(rigidbody.rotation, targetRot, rotSpeed * Time.deltaTime);
-        rigidbody.MoveRotation(resultRot);
+        var targetRot = Quaternion.LookRotation(_direction, Vector3.up);
+        var resultRot = Quaternion.Slerp(_rigidbody.rotation, targetRot, rotSpeed * Time.deltaTime);
+        _rigidbody.MoveRotation(resultRot);
     }
+}
+
+public enum MovementState
+{
+    walking,
+    sprinting
 }
